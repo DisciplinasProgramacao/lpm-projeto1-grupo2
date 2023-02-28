2d8c5b55c00ca103c08da545686ed867da16a2c7
miguelmfc <martinsmiguelc@gmail.com>
2023-02-28 18:56:30 -0300

    Update ItemVendido.java

--- codigo/src/ItemVendido.java
+++ codigo/src/ItemVendido.java
@@ -1,8 +1,17 @@
 public class ItemVendido {
 
 	private int quant;
+	private Produto p;
+
+	public int getQuant (){
+		return this.quant;
+	}
+	public void setQuant (int q){
+		this.quant= q;
+	}
 
 	public void getValorTotal() {
+		return quant*p.getValorTotal();
 
 	}
 
