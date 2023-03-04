3649e466dc6a9b2ca4df9a09da73b74eb7d0b819
lucasrossif <lucasfigueira12@gmail.com>
2023-03-03 22:20:51 -0300

    update Produto, diagramas de classes

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -1,6 +1,13 @@
 public class App {
     public static void main(String[] args) {
-        Produto p = new Produto("Bola", 10, 0.5);
+        Produto p1 = Produto.registrarProduto("Bo", 89, 10, 0.5);
+        Produto p2 = Produto.registrarProduto("Arroz 1kg", 10,11, 0.59);
+        Produto p3 = Produto.registrarProduto("Feijão 1kg", 12,15, 0.6);
+        Produto p4 = Produto.registrarProduto("Biscoito salgado", 15,8, 0.42);
+        Produto p5 = Produto.registrarProduto("Biscoito doce", 5,5, 0.34);
+
+        System.out.println(p2.getCustoTotal());
+
 
     }
 }
--- codigo/src/Produto.java
+++ codigo/src/Produto.java
@@ -1,5 +1,7 @@
 
 public class Produto {
+	private static int ultimoCodigo;
+	private int codigo;
 	private String descricao;
 	private static final int MIN_CARACTERES_DESCRICAO = 3;
 	private static double MIN_MARGEM_LUCRO = 0.3;
@@ -7,46 +9,68 @@ public class Produto {
 	private double precoCusto;
 	private double valorVenda;
 	private double margemLucro;
-	private int quant;
-	private static double IMPOSTO = 1.18;
-	
-	public Produto(String descricao, double precoCusto, double margemLucro){
-		
+	private int qtdInicial;
+	private static double IMPOSTO = 0.18;
+
+	/*
+	 * Construtor de Produto, que é chamado pelo factory method registrarProduto()
+	 * São feitas as validações do tamanho de caracteres da descrição, da margem de lucro
+	 * mínima e máxima e do valor de venda, a partir do preço de custo, margem de lucro e
+	 * impostos. Se a criação for bem sucedida, uma mensagem com alguns dados do produto é
+	 * retornada. Senão, a exceção é capturada e exibida ao usuário (ex.: margem de lucro
+	 * inválida).
+	 */
+	private Produto(String descricao, int qtdInicial, double precoCusto, double margemLucro){
+
 		try {
 			validarCaracteresProduto(descricao);
 			validaMargemLucro(margemLucro);
 
+			//Cria automaticamente o código do produto de acordo com o último código criado.
+			this.codigo = Produto.ultimoCodigo + 1;
+			Produto.ultimoCodigo = this.codigo;
+
 			this.descricao = descricao;
+			this.qtdInicial = qtdInicial;
 			this.precoCusto = precoCusto;
 			this.margemLucro = margemLucro;
 			this.valorVenda = calcularPrecoVenda(precoCusto, margemLucro);
 
-			System.out.println("O produto '" + descricao + "' foi registrado.\nValor de venda: R$ " + valorVenda);
+			System.out.printf("O produto '%s' foi registrado com o código %d." +
+					"\n- Custo: R$ %.2f" +
+					"\n- Qtd. inicial: %d" +
+					"\n- Custo total: R$ %.2f" +
+					"\n- Margem de lucro: %.1f%%" +
+					"\n- Valor dos impostos: R$ %.2f" +
+					"\n- Valor de venda: R$ %.2f\n",
+					descricao, this.codigo, precoCusto, qtdInicial, (precoCusto * qtdInicial),
+					(margemLucro * 100), this.getValorImposto(), valorVenda);
+			System.out.println("----------------------");
+
 		} catch (Exception e) {
-			System.out.print(e);
+			String errorMsg = e.getMessage();
+			System.out.printf("\nErro ao cadastrar o produto '%s': %s\n", descricao, errorMsg);
+			System.out.println("----------------------");
 		}
 
 	}
-
 	/*
-	 * Método que registra produto
+	 * Factory method responsável por criar um novo produto, de forma que
+	 * o método de criação tenha um nome mais amigável.
 	 */
-//	public String registrarProduto() {
-//
-//        StringBuilder registraPedido = new StringBuilder("Produto: "+this.nome+"\n");
-//        registraPedido.append("Quantidade: " +this.quant+"\n");
-//        registraPedido.append("\nValor: R$ " + this.precoVenda(precoCusto));
-//        registraPedido.append("\nDescrição: R$ " + this.validarCaracteresProduto());
-//        return registraPedido.toString();
-//	}
-//
+	static Produto registrarProduto(String descricao, int qtdInicial, double precoCusto, double margemLucro){
+		return new Produto(descricao, qtdInicial, precoCusto, margemLucro);
+	}
+
 	/*
 	 * Método que calcula o valor do imposto de determinado produto
 	 */
-//	public double getValorImposto() {
-//		return (this.precoCusto + this.margemLucro(precoCusto))/ IMPOSTO;
-//	}
-	
+	public double getValorImposto() {
+		double valorImposto = this.valorVenda * IMPOSTO;
+
+		return valorImposto;
+	}
+
 	/*
 	 * Método para validar quantidade de caracteres na descrição de um produto
 	 */
@@ -55,38 +79,55 @@ public class Produto {
 			throw new IllegalArgumentException("Descrição Inválida!\nNúmero de caracteres menor que 3. ");
 		}
 	}
-	
+
 	/*
-	 * Método que calcula o preço da Venda de um produto 
+	 * Método que calcula o preço da venda de um produto
+	 * a partir do preço de custo e da margem de lucro desejada
 	 */
 	public double calcularPrecoVenda(double precoCusto, double margemLucro) {
 
-		return (precoCusto + (margemLucro * precoCusto)) * IMPOSTO ;
+		double valorSemImposto = precoCusto + (margemLucro * precoCusto);
+		double precoVenda = valorSemImposto + (IMPOSTO * valorSemImposto);
+
+		return precoVenda;
 	}
-	
-	/*
-	 * Método que verifica se o cálculo da margem é válida(a partir de uma fórmula). Se for válida, retorna a margem de Lucro. 
-	 */
-//	public double calculaMargemLucro(double precoCusto) {
-//		double calculoMargem = (this.precoVenda(precoCusto) - this.precoCusto)/this.precoVenda(precoCusto);
-//		if (calculoMargem >= MIN_MARGEM_LUCRO || calculoMargem >= MAX_MARGEM_LUCRO) {
-//			return calculoMargem;
-//		} else {
-//			throw new IllegalArgumentException("Margem de lucro inválida!");
-//		}
-//	}
 
 	public void validaMargemLucro(double margemLucro) {
 		if (margemLucro < MIN_MARGEM_LUCRO || margemLucro > MAX_MARGEM_LUCRO) {
-			throw new IllegalArgumentException("Margem de lucro baixa! Deve ser entre 30% e 80%");
+			throw new IllegalArgumentException("Margem de lucro baixa! Deve ser entre 30% e 80%. " +
+					"Foi informado " + (margemLucro*100) + "%.");
 		}
 	}
-	
+
+	/*
+	 * Método que retorna a descrição do produto
+	 */
 	public String getDescricao() {
 		return this.descricao;
 	}
-	public int getQuantidade() {
-		return quant;
+
+	/*
+	 * Método que retorna a quantidade inicial de produtos cadastrados
+	 */
+	public int getQuantidadeInicial() {
+		return qtdInicial;
 	}
+
+	/*
+	 * Método que calcula o custo total, a partir da quantidade
+	 * de itens e do valor de custo. A consulta é feita diretamente pelo estoque
+	 *
+	 * TODO
+	 * Consultar a quantidade pelo estoque ao invés da qtd. inicial.
+	 */
+	public double getCustoTotal(){
+		double custoTotal = this.qtdInicial * this.precoCusto;
+
+		return custoTotal;
+
+
+	}
+
+
 
 }
