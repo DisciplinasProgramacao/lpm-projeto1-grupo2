28176e3a2cd8646bff0c817a5f723f5e3922358c
AnaFlaviaRibeiro <103009514+AnaFlaviaRibeiro@users.noreply.github.com>
2023-03-05 23:40:31 -0300

    Adicionando linhas de código comentadas na classe ProdutoTest

--- codigo/src/ProdutoTest.java
+++ codigo/src/ProdutoTest.java
@@ -1,27 +1,28 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class ProdutoTest {
 	private Produto produto;
-	
+
 	@BeforeEach
 	void init() {
 		produto = Produto.registrarProduto("Lápis", 10, 5.0, 0.5);
 	}
-	
-	@Test 
+
+	@Test
 	void testandoValorImposto() {
 		Assertions.assertEquals(1.593, produto.getValorImposto());
 	}
-//	@Test
-//    void testandoValidacaoDeCaractetesDoProduto() {
-//        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-//           produto = Produto.registrarProduto("La", 10, 5.0, 0.5);
-//        });
-//    }
-	
+
+	@Test
+    void testandoValidacaoDeCaractetesDoProduto() {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () -> Produto.registrarProduto("La", 10, 5.0, 0.5));
+		assertEquals("Descrição Inválida!\nNúmero de caracteres menor que 3. ", exception.getMessage());
+	}
 //    @Test
 //    void testandoValidacaoMargemDeLucro() {
 //        Assertions.assertThrows(IllegalArgumentException.class, () -> {
@@ -32,6 +33,7 @@ class ProdutoTest {
 	void testantoCalcularPrecoVenda() {
 		Assertions.assertEquals(8.85, produto.calcularPrecoVenda(5.0, 0.5));
 	}
+
 	@Test
 	void testandoCustoTotal() {
 		Assertions.assertEquals(50.0, produto.getCustoTotal());
