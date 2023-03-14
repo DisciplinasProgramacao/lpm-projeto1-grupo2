b004734bac9fba7e8226bdb2add5ab5a22af147d
lucasrossif <lucasfigueira12@gmail.com>
2023-03-14 11:16:00 -0300

    Update ItemVendido

--- codigo/src/ItemVendido.java
+++ codigo/src/ItemVendido.java
@@ -2,20 +2,22 @@ public class ItemVendido {
 
 	private int quant;
 	private Produto produto;
-	private double valorTotalItem;
 
+	//Construtor que recebe um Produto e a quantidade de itens desse Produto na Venda
 	public ItemVendido(Produto produto, int quant){
 		this.produto = produto;
 		this.quant = quant;
 	}
+
+	//Retorna o valor total de um Produto na Venda, de acordo com sua quantidade e valor de venda
 	public double getValorTotal() {
 		return this.quant * this.produto.getValorVenda();
-
 	}
 	//Retorna o produto relacionado ao ItemVendido em questão
 	public Produto getProdutoItem(){
 		return this.produto;
 	}
+	//Retorna a quantidade de itens de um Produto na Venda
 	public int getQuantItem(){
 		return this.quant;
 	}
@@ -23,4 +25,5 @@ public class ItemVendido {
 	public void addQuantItem(int quant){
 		this.quant += quant;
 	}
+
 }
